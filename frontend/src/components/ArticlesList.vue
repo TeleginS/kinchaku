8e57c9c5c1840ff99770bbf20c39b97f169d48db
Sergei Telegin <sergei@macroactive.com>
2021-12-18 13:47:03 +1000

    routes + components

---
+++ frontend/src/components/ArticlesList.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="list row">
+  </div>
+</template>
+<script>
+export default {}
+</script>
---
+++ frontend/src/components/UploadArticles.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="submit-form">
+  </div>
+</template>
+<script>
+export default {}
+</script>
